5e18c9db20bda9362f76086851750525c89a6a61
justusmattern <justus_mattern@gmx.de>
2022-05-31 19:02:57 +0200

    evaluation procedure civilcomments

--- train_civilcomments.py
+++ train_civilcomments.py
@@ -10,6 +10,55 @@ import os
 from tqdm import tqdm
 from wilds.common.data_loaders import get_eval_loader, get_train_loader
 from wilds import get_dataset
+from sklearn.metrics import accuracy_score
+
+
+def meta_to_domain(meta):
+    domain_list = []
+    domain_verbalizers = ['male', 'female', 'LGBTQ', 'christian', 'muslim', 'differently religious', 'black', 'white']
+
+    for dom_list in meta:
+        domains = []
+        for i in range(8):
+            if dom_list[i] == 1:
+                domains.append(domain_verbalizers[i])
+
+        domain_list.append(domains)
+
+    return domain_list
+
+
+def get_domain_indices(domains_list):
+    domain_indices = dict()
+    domain_verbalizers = ['male', 'female', 'LGBTQ', 'christian', 'muslim', 'differently religious', 'black', 'white']
+    for domain in domain_verbalizers:
+        domain_indices[domain] = []
+
+    for domains in domains_list:
+        for dom in domain_verbalizers:
+            if dom in domains:
+                domain_indices[dom].append(True)
+            else:
+                domain_indices[dom].append(False)
+
+    return domain_indices
+
+
+def domain_evaluation(domain_indices, predictions, labels):
+    domain_verbalizers = ['male', 'female', 'LGBTQ', 'christian', 'muslim', 'differently religious', 'black', 'white']
+
+    for domain in domain_verbalizers:
+        print(f'evaluating for domain {domain}')
+
+        domain_preds_toxic = [p for p, i, l in zip(predictions, domain_indices[domain], labels) if i and l==1]
+        domain_labels_toxic = [1]*len(domain_preds_toxic)
+
+        domain_preds_neutral = [p for p, i, l in zip(predictions, domain_indices[domain], labels) if i and l==1]
+        domain_labels_neutral = [0]*len(domain_preds_neutral)
+
+        print('accuracy for toxic', accuracy_score(domain_preds_toxic, domain_labels_toxic))
+        print('accuracy for neutral', accuracy_score(domain_preds_neutral, domain_labels_neutral))
+
 
 def lm_loss(model, input, loss_fn):
     logits = model(input_ids=input).logits.permute(0,2,1)
@@ -53,6 +102,8 @@ def forward_pass(x, y, model, tokenizer, prompts, loss_fn_lm, loss_fn_cls, causa
 
 
 def train(train_file: str, test_file: str, batch_size: int, model_name: str, tokenizer_name: str, prompts: list, num_epochs: int, causal:bool):
+    domain_verbalizers = ['male', 'female', 'LGBTQ', 'christian', 'muslim', 'differently religious', 'black', 'white']
+
     dataset = get_dataset(dataset="civilcomments", download=True)
     train_data = dataset.get_subset("train")
     train_loader = get_train_loader("standard", train_data, batch_size=1)
@@ -82,7 +133,14 @@ def train(train_file: str, test_file: str, batch_size: int, model_name: str, tok
         test_preds = []
         test_true = []
         test_meta = []
+        all_domains = []
+        domain_indices = dict()
+        for dom in domain_verbalizers:
+            domain_indices[dom] = []
+
+
         for x, y, meta in tqdm(test_loader):
+            domains_list = meta_to_domain(meta)
             loss, label_probs = forward_pass(x, y, model, tokenizer, prompts, loss_fn_lm, loss_fn_cls, causal)
             
             preds = torch.argmax(label_probs, dim=1)
@@ -92,8 +150,14 @@ def train(train_file: str, test_file: str, batch_size: int, model_name: str, tok
             test_true.append(y[0].cpu().item())
             test_meta.append(meta[0].cpu())
 
+            local_domain_indices = get_domain_indices(domains_list)
+            for dom in domain_verbalizers:
+                domain_indices[dom].extend(local_domain_indices[dom])
+
         logging.info('testing accuracy'+str(test_acc/len(test_data)))
         print('testing accuracy', test_acc/len(test_data))
+        domain_evaluation(domain_indices, test_preds, test_true)
+        
         model.save_pretrained(f'model_imdb_epoch_{epoch}.pt')
 
         train_acc = 0
